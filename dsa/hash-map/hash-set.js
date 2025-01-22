7b2f207b31db606ffe4320601c5293ff264b122d
srj-17 <saugatrijal17@gmail.com>
2025-01-22 15:48:23 +0545

    Implement Hash Set
    Just some tweaking on hash map
    gave me hash set,
    Remove the values, and consider
    hash set as a hashmap without values

--- dsa/hash-map/hash-set.js
+++ dsa/hash-map/hash-set.js
@@ -1,7 +1,8 @@
 import LinkedList from "../linked-list/linked-list.js";
 
-function HashMap() {
+function HashSet() {
     const loadFactor = 0.75;
+
     // we can't do buckets.length, because that just keeps changing in js
     let capacity = 16;
     let entryCount = 0;
@@ -24,11 +25,14 @@ function HashMap() {
             if (index < 0 || index >= buckets.length) {
                 throw new Error("Trying to access index out of bounds");
             }
+
+            if (!buckets[index]) return false;
+
             const linkedListHead = buckets[index].head;
 
             let temp = linkedListHead;
             while (temp !== null) {
-                if (temp.value.key === key) {
+                if (temp.value === key) {
                     return true;
                 }
 
@@ -42,22 +46,6 @@ function HashMap() {
         }
     }
 
-    function update(key, value) {
-        const index = hash(key);
-        const linkedListHead = buckets[index].head;
-
-        let temp = linkedListHead;
-        while (temp !== null) {
-            if (temp.value.key === key) {
-                temp.value.value = value;
-                return;
-            }
-
-            temp = temp.next;
-        }
-    }
-
-    // we don't need to copy the array to a new array because
     // in javaScript, array can grow, so we can just increase its capacity
     function updateCapacity() {
         capacity = capacity * 2;
@@ -77,11 +65,11 @@ function HashMap() {
         return hashCode;
     }
 
-    function set(key, value) {
+    function set(key) {
         const index = hash(key);
 
         // prevent javaScript's dynamic array length for buckets
-        // for the purposes of hash map learning
+        // for the purposes of hash set learning
         if (index < 0 || index >= capacity) {
             throw new Error("Trying to access index out of bounds");
         }
@@ -93,9 +81,9 @@ function HashMap() {
 
         // check if the key already exists
         if (has(key)) {
-            update(key, value);
+            return;
         } else {
-            buckets[index].append({ key, value });
+            buckets[index].append(key);
             // increase number of entires if we append one
             entryCount++;
         }
@@ -116,10 +104,15 @@ function HashMap() {
 
             // get the linked list for the index and traverse through it
             // to get the value
+            if (!buckets[index]) {
+                return null;
+            }
+
             let temp = buckets[index].head;
+
             while (temp !== null) {
-                if (temp.value.key === key) {
-                    return temp.value.value;
+                if (temp.value === key) {
+                    return temp.value;
                 }
 
                 temp = temp.next;
@@ -141,7 +134,7 @@ function HashMap() {
         let linkedListHead = buckets[index].head;
 
         // if value in the head
-        if (linkedListHead.value.key === key) {
+        if (linkedListHead.value === key) {
             buckets[index].head = linkedListHead.next;
 
             entryCount--;
@@ -153,7 +146,7 @@ function HashMap() {
         let temp = linkedListHead;
 
         while (temp.next !== null) {
-            if (temp.next.value.key === key) {
+            if (temp.next.value === key) {
                 temp.next = temp.next.next;
                 entryCount--;
                 return true;
@@ -178,27 +171,12 @@ function HashMap() {
             const linkedListHead = bucket.head;
             let temp = linkedListHead;
             while (temp !== null) {
-                keys = keys.concat(temp.value.key);
+                keys = keys.concat(temp.value);
                 temp = temp.next;
             }
         });
 
         return keys;
-    }
-
-    function values() {
-        let values = [];
-        buckets.forEach((bucket) => {
-            if (bucket === null) return;
-            const linkedListHead = bucket.head;
-            let temp = linkedListHead;
-            while (temp !== null) {
-                values = values.concat(temp.value.value);
-                temp = temp.next;
-            }
-        });
-
-        return values;
     }
 
     function entries() {
@@ -208,9 +186,7 @@ function HashMap() {
             const linkedListHead = bucket.head;
             let temp = linkedListHead;
             while (temp !== null) {
-                entries = entries.concat({
-                    [temp.value.key]: temp.value.value,
-                });
+                entries = entries.concat(temp.value);
                 temp = temp.next;
             }
         });
@@ -226,7 +202,6 @@ function HashMap() {
         length,
         clear,
         keys,
-        values,
         entries,
         getCapacity,
         getLoadLevel,
@@ -234,51 +209,50 @@ function HashMap() {
 }
 
 // odin test
-const test = HashMap();
+const test = HashSet();
 
-// populating the map
-test.set("apple", "red");
-test.set("banana", "yellow");
-test.set("carrot", "orange");
-test.set("dog", "brown");
-test.set("elephant", "gray");
-test.set("frog", "green");
-test.set("grape", "purple");
-test.set("hat", "black");
-test.set("ice cream", "white");
-test.set("jacket", "blue");
-test.set("kite", "pink");
-test.set("lion", "golden");
+// populating the set
+test.set("apple");
+test.set("banana");
+test.set("carrot");
+test.set("dog");
+test.set("elephant");
+test.set("frog");
+test.set("grape");
+test.set("hat");
+test.set("ice cream");
+test.set("jacket");
+test.set("kite");
+test.set("lion");
 console.log(test.length());
 
 // updating the values
 console.log("Below load levels (on the border 0.75)");
-test.set("lion", "yellow");
+test.set("lion");
 console.log(`Entries count: ${test.length()}`);
 console.log(`Capacity: ${test.getCapacity()}`);
 console.log(`Load Level: ${test.getLoadLevel()}`);
 
 // testing setting over load limit
-console.log("\nAfter adding additional element to hash map");
+console.log("\nAfter adding additional element to hash set");
 test.set("moon", "silver");
 console.log(`Entries count: ${test.length()}`);
 console.log(`Capacity: ${test.getCapacity()}`);
 console.log(`Load Level: ${test.getLoadLevel()}`);
 
-// additional test
-console.log("\nAdditional test: ");
-test.set("Rama", "hello");
-test.set("Rama", "hellB");
-test.set("Sita", "hello");
-test.set("gita", "hello");
-test.set("Rita", "hari");
-console.log(test.get("Rama"));
-console.log(test.get("Ram"));
-console.log(test.has("Rama"));
-console.log(test.has("Ram"));
-console.log(test.remove("Rama"));
-console.log(test.remove("gita"));
-console.log(test.length());
-console.log(test.keys());
-console.log(test.values());
-console.log(test.entries());
+//// additional test
+console.log("\nAdditional test: \n");
+test.set("Rama");
+test.set("Rama");
+test.set("Sita");
+test.set("gita");
+test.set("Rita");
+console.log(`Get Rama: ${test.get("Rama")}`);
+console.log(`Get Ram: ${test.get("Ram")}`);
+console.log(`Has rama: ${test.has("Rama")}`);
+console.log(`Has ram: ${test.has("Ram")}`);
+console.log(`Remove Rama: ${test.remove("Rama")}`);
+console.log(`Remove Gita: ${test.remove("gita")}`);
+console.log(`Length: ${test.length()}`);
+console.log(`Keys: ${test.keys()}`);
+console.log(`Entries: ${test.entries()}`);
