c5a5c56a744b3b31578ed80d80c2024e190d768d
srj-17 <saugatrijal17@gmail.com>
2025-02-19 06:28:33 +0545

    feat: Implement decryption for rail fence

--- cryptography/rail-fence-cipher/rf.cpp
+++ cryptography/rail-fence-cipher/rf.cpp
@@ -41,6 +41,54 @@ string encrypt(string text, int key) {
   return result;
 }
 
+string decrypt(string text, int key) {
+  // first build a fence with depth = key
+  // length = text length
+  const int fence_width = text.length();
+  const int fence_height = key;
+  char fence[fence_height][fence_width];
+  const int textLength = text.length();
+
+  // build the fence
+  for (int i = 0; i < fence_height; i++)
+    for (int j = 0; j < fence_width; j++)
+      fence[i][j] = '\n';
+
+  bool directionDown = false;
+  int row = 0, col = 0;
+  // place * in the positions where ciphertext will go
+  for (int i = 0; i < fence_width; i++) {
+    if (row == 0 || row == key - 1)
+      directionDown = !directionDown;
+
+    fence[row][col++] = '*';
+
+    directionDown ? row++ : row--;
+  }
+
+  // put ciphertext in those positions
+  int index = 0;
+  for (int i = 0; i < fence_height; i++)
+    for (int j = 0; j < fence_width; j++)
+      if (fence[i][j] == '*' && index < textLength)
+        fence[i][j] = text[index++];
+
+  // find plainText
+  string result;
+  directionDown = false, row = 0, col = 0;
+  for (int i = 0; i < textLength; i++) {
+    if (row == 0 || row == fence_height - 1)
+      directionDown = !directionDown;
+
+    if (fence[row][col] != '*')
+      result.push_back(fence[row][col++]);
+
+    directionDown ? row++ : row--;
+  }
+
+  return result;
+}
+
 int main() {
   string plainText, cipherText;
   int key;
@@ -50,6 +98,7 @@ int main() {
   cin >> key;
   cipherText = encrypt(plainText, key);
   cout << "The cipher text is: " << cipherText << endl;
+  cout << "Back to plainText: " << decrypt(cipherText, key) << endl;
 
   return 0;
 }
