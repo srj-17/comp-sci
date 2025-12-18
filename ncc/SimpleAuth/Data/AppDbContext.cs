f61a5c813a3bce9e7fbbabe085793ca0be373152
srj-17 <saugatrijal17@gmail.com>
2025-12-18 18:27:04 +0545

    cleanup after the last commit

--- ncc/SimpleAuth/Data/AppDbContext.cs
+++ ncc/SimpleAuth/Data/AppDbContext.cs
@@ -1,13 +1,10 @@
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace SimpleAuth.Data
 {
-    // Database Context
-    public class AppDbContext : IdentityDbContext<IdentityUser>
+    public class AppDbContext : IdentityDbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
     }
 }
-
